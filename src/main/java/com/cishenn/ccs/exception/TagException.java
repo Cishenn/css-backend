997979096f2396c6c94832ce2600a8c958abccb6
恶魔猎手但丁 <457129810@qq.com>
2020-06-30 16:00:33 +0800

    Dante/tag

---
+++ src/main/java/com/cishenn/ccs/exception/TagException.java
@@ -0,0 +1,7 @@
+package com.cishenn.ccs.exception;
+
+public class TagException extends BaseException{
+    public TagException(String message){
+        super("VisitorInfo Part Error", message);
+    }
+}
